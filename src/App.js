a2640dafcebef222da9bed69d5049daa2a437f2a
Christina Williams <christinaxtwilliams@gmail.com>
2020-10-29 13:11:32 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React from 'react';
+import axios from 'axios';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    facts: []
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:3001/quiz-info')
+    .then((response) => {
+      const facts = response.data;
+
+      this.setState({
+        facts
+      })
+      // console.log(this.state.facts);
+    })
+    .catch(error => {
+      console.log(error);
+      console.log('An error has occurred...');
+    })
+  }
+
+  render() {
+    return (
+      <div>
+      <h1>Game</h1>
+        {this.state.facts.map((fact, index) => (
+          <div key={index}>
+            <h3>{fact.question}</h3>
+          </div>
+        ))}
+      </div>
+    )
+  }
 }
 
 export default App;
