17713d07b152870b7bfd84603012a1d18d9b6728
Christina Williams <christinaxtwilliams@gmail.com>
2020-10-29 19:31:48 -0400

    added

--- src/App.js
+++ src/App.js
@@ -29,19 +29,20 @@ class App extends React.Component {
 
   render() {
     return (
+  
       <div className='container'>
 
-        <div className='bg-black text-purple-500 p-10 rounded-lg shadow-mg'>
+        <div className='bg-black text-purple-500 p-10 rounded-lg shadow-mg w-full'>
           <h2 className='text-3xl'>
             question
           </h2>
            </div>
 
        <div className='mt-5'>
-        <button className='bg-green-500 hover:bg-purple-500 w-1/2 p-4 text-black-800 font-bold py-2 px-4 border border-black rounded-lg shadow-mg'>answer</button>
-        <button className='bg-green-500 hover:bg-purple-500 w-1/2 p-4 text-black-800 font-bold py-2 px-4 border border-black rounded-lg shadow-mg'>answer</button>
-        <button className='bg-green-500 hover:bg-purple-500 w-1/2 p-4 text-black-800 font-bold py-2 px-4 border border-black rounded-lg shadow-mg'>answer</button>
-        <button className='bg-green-500 hover:bg-purple-500 w-1/2 p-4 text-black-800 font-bold py-2 px-4 border border-black rounded-lg shadow-mg'>answer</button>
+        <button className='bg-green-500 hover:bg-purple-500 w-1/2 p-4 text-black-800 font-bold py-2 px-4 border border-black rounded-lg shadow-mg mb-4'>answer</button>
+        <button className='bg-green-500 hover:bg-purple-500 w-1/2 p-4 text-black-800 font-bold py-2 px-4 border border-black rounded-lg shadow-mg mb-4'>answer</button>
+        <button className='bg-green-500 hover:bg-purple-500 w-1/2 p-4 text-black-800 font-bold py-2 px-4 border border-black rounded-lg shadow-mg mb-4'>answer</button>
+        <button className='bg-green-500 hover:bg-purple-500 w-1/2 p-4 text-black-800 font-bold py-2 px-4 border border-black rounded-lg shadow-mg mb-4'>answer</button>
 
        </div>
       </div>
