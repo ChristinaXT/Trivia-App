976478be4538c848cbe57101a4878c7e3f8f6111
Christina Williams <christinaxtwilliams@gmail.com>
2020-10-30 17:27:04 -0400

    added

--- src/components/Trivia.js
+++ src/components/Trivia.js
@@ -1,21 +1,21 @@
 import React from 'react';
 
 const Button = ({ answer }) => (
-  <button className='bg-yellow-500 hover:bg-purple-500 w-1/2 p-4
-  text-black-800 font-bold py-2 px-4 border border-black
-  rounded-lg shadow-md mb-4'>
-   {answer}
+  <button className='bg-yellow-500 hover:bg-purple-500 p-4
+  text-black-800 font-bold border border-black
+  rounded-lg shadow-md'>
+      {answer}
   </button>
 );
 
-const Trivia = ({ data: {question, correct, incorrect} }) => (
+const Trivia = ({ data: { question, correct, incorrect } }) => (
   <div>
-  <div className='bg-black text-purple-500 p-10
-  rounded-lg shadow-md'>
-    <h2 className='text-3xl'>
-      {question}
-    </h2>
-  </div>
+    <div className='bg-black text-purple-500 p-10
+      rounded-lg shadow-md'>
+       <h2 className='text-3xl'>
+        {question}
+       </h2>
+    </div>
 
    <div className='grid grid-cols-2 gap-6 mt-6'>
      <button answer={correct} />
