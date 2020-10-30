dd74abd284c1a22db3bcd237093901b7daa68a57
Christina Williams <christinaxtwilliams@gmail.com>
2020-10-29 21:19:08 -0400

    adding hardcode

--- src/App.js
+++ src/App.js
@@ -1,51 +1,50 @@
-import React from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 
 
-class App extends React.Component {
-  state = {
-    facts: []
-  }
 
-  componentDidMount() {
-    const url = 'http://localhost:3001/quiz-info';
+const url = 'http://localhost:3001/quiz-info';
 
-    axios.get(url)
-    .then((response) => {
-      const facts = response.data;
+function App() {
+  const [questions, setQuestions] = useState([]);
 
-      this.setState({
-        facts
-      })
-      // console.log(this.state.facts);
-    })
-    .catch(error => {
-      console.log(error);
-      console.log('An error has occurred...');
-    })
-  }
+useEffect(() => {
+  fetch(url)
+    .then((res) => res.json())
+    .then((facts) => {
+     setQuestions(facts.results);
+    });
+}, []);
 
-  render() {
     return (
-
       <div className='container'>
-
+         { questions.length > 0 }
         <div className='bg-black text-purple-500 p-10 rounded-lg shadow-mg'>
           <h2 className='text-3xl'>
-            question
+            {questions[0].question}
           </h2>
            </div>
 
        <div className='mt-5'>
-        <button className='bg-green-500 hover:bg-purple-500 w-1/2 p-4 text-black-800 font-bold py-2 px-4 border border-black rounded-lg shadow-mg mb-4'>answer</button>
-        <button className='bg-green-500 hover:bg-purple-500 w-1/2 p-4 text-black-800 font-bold py-2 px-4 border border-black rounded-lg shadow-mg mb-4'>answer</button>
-        <button className='bg-green-500 hover:bg-purple-500 w-1/2 p-4 text-black-800 font-bold py-2 px-4 border border-black rounded-lg shadow-mg mb-4'>answer</button>
-        <button className='bg-green-500 hover:bg-purple-500 w-1/2 p-4 text-black-800 font-bold py-2 px-4 border border-black rounded-lg shadow-mg mb-4'>answer</button>
-
+        <button className='bg-green-500 hover:bg-purple-500 w-1/2 p-4
+        text-black-800 font-bold py-2 px-4 border border-black
+        rounded-lg shadow-mg mb-4'>{questions[0].correct_answer}
+        </button>
+        <button className='bg-green-500 hover:bg-purple-500 w-1/2 p-4
+        text-black-800 font-bold py-2 px-4 border border-black
+        rounded-lg shadow-mg mb-4'>{questions[0].incorrect_answer}
+        </button>
+        <button className='bg-green-500 hover:bg-purple-500 w-1/2 p-4
+        text-black-800 font-bold py-2 px-4 border border-black
+        rounded-lg shadow-mg mb-4'>{questions[0].incorrect_answer}
+        </button>
+        <button className='bg-green-500 hover:bg-purple-500 w-1/2 p-4
+        text-black-800 font-bold py-2 px-4 border border-black
+        rounded-lg shadow-mg mb-4'>{questions[0].incorrect_answer}
+        </button>
        </div>
       </div>
     )
   }
-}
+
 
 export default App;
