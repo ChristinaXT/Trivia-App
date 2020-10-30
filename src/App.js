df4c6ffa3b823a2ee68a62e734065395ac77add7
Christina Williams <christinaxtwilliams@gmail.com>
2020-10-30 16:01:41 -0400

    fixed errors

--- src/App.js
+++ src/App.js
@@ -6,26 +6,73 @@ const url = 'http://localhost:3001/quiz-info';
 const App = () => {
   const [questions, setQuestions] = useState([]);
 
-useEffect(() => {
-  fetch(url)
-    .then((res) => res.json())
-    .then((facts) => {
-     setQuestions(facts.results);
-    });
-}, []);
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+       setQuestions(data);
+       // console.log(data);
+       // console.log(data[1].question);
+      });
+  }, []);
 
-   // if(!questions.length) {
-   //   return null;
-   // }
+   if(!questions.length) {
+     return null;
+   }
 
-    return questions.length > 0 ? (
+    return (
+      <div>
+      {
+
+        questions.length > 0
+        ?
+        (
+          <div className='container'>
+            <div className='bg-black text-purple-500 p-10
+            rounded-lg shadow-md'>
+              <h2 className='text-3xl'>
+                {questions[0].question}
+              </h2>
+            </div>
+
+           <div className='mt-5'>
+            <button className='bg-yellow-500 hover:bg-purple-500 w-1/2 p-4
+            text-black-800 font-bold py-2 px-4 border border-black
+            rounded-lg shadow-md mb-4'>{questions[0].correct}
+            </button>
+            <button className='bg-yellow-500 hover:bg-purple-500 w-1/2 p-4
+            text-black-800 font-bold py-2 px-4 border border-black
+            rounded-lg shadow-md mb-4'>{questions[0].incorrect}
+            </button>
+            <button className='bg-yellow-500 hover:bg-purple-500 w-1/2 p-4
+            text-black-800 font-bold py-2 px-4 border border-black
+            rounded-lg shadow-md mb-4'>{questions[0].incorrect}
+            </button>
+            <button className='bg-yellow-500 hover:bg-purple-500 w-1/2 p-4
+            text-black-800 font-bold py-2 px-4 border border-black
+            rounded-lg shadow-md mb-4'>{questions[0].incorrect}
+            </button>
+           </div>
+         </div>
+       )
+       :
+       (
+          <h1>....Loading!</h1>
+        )
+
+      }
+      </div>
+    )
+
+
+    {/* return questions.length > 0 ? (
       <div className='container'>
         <div className='bg-black text-purple-500 p-10
         rounded-lg shadow-md'>
           <h2 className='text-3xl'
             {questions[0].question}
           />
-           </div>
+        </div>
 
        <div className='mt-5'>
         <button className='bg-green-500 hover:bg-purple-500 w-1/2 p-4
@@ -48,7 +95,7 @@ useEffect(() => {
      </div>
    ) : (
         <h1>....Loading!</h1>
-       )
+      ) */}
 
   }
 
