18499897c55a3a2f33b72d218e6dabd4cb1828f1
Christina Williams <christinaxtwilliams@gmail.com>
2020-10-30 17:10:43 -0400

    added

--- src/components/Trivia.js
+++ src/components/Trivia.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
+const Button = ({ answer }) => (
+  <button className='bg-yellow-500 hover:bg-purple-500 w-1/2 p-4
+  text-black-800 font-bold py-2 px-4 border border-black
+  rounded-lg shadow-md mb-4'>
+   {answer}
+  </button>
+);
+
 const Trivia = ({ data: {question, correct, incorrect} }) => (
   <div>
   <div className='bg-black text-purple-500 p-10
@@ -9,23 +17,11 @@ const Trivia = ({ data: {question, correct, incorrect} }) => (
     </h2>
   </div>
 
-   <div className='mt-5'>
-    <button className='bg-yellow-500 hover:bg-purple-500 w-1/2 p-4
-    text-black-800 font-bold py-2 px-4 border border-black
-    rounded-lg shadow-md mb-4'>{correct}
-    </button>
-    <button className='bg-yellow-500 hover:bg-purple-500 w-1/2 p-4
-    text-black-800 font-bold py-2 px-4 border border-black
-    rounded-lg shadow-md mb-4'>{incorrect[0]}
-    </button>
-    <button className='bg-yellow-500 hover:bg-purple-500 w-1/2 p-4
-    text-black-800 font-bold py-2 px-4 border border-black
-    rounded-lg shadow-md mb-4'>{incorrect[1]}
-    </button>
-    <button className='bg-yellow-500 hover:bg-purple-500 w-1/2 p-4
-    text-black-800 font-bold py-2 px-4 border border-black
-    rounded-lg shadow-md mb-4'>{incorrect[2]}
-    </button>
+   <div className='grid grid-cols-2 gap-6 mt-6'>
+   <button answer={correct} />
+   <button answer={incorrect[0]} />
+   <button answer={incorrect[1]} />
+   <button answer={incorrect[2]} />
    </div>
  </div>
 )
