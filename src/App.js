0eb092221baee954eda6ef5a170909d03813e29f
Christina Williams <christinaxtwilliams@gmail.com>
2020-10-30 16:13:57 -0400

    added

--- src/App.js
+++ src/App.js
@@ -64,39 +64,6 @@ const App = () => {
       </div>
     )
 
-
-    {/* return questions.length > 0 ? (
-      <div className='container'>
-        <div className='bg-black text-purple-500 p-10
-        rounded-lg shadow-md'>
-          <h2 className='text-3xl'
-            {questions[0].question}
-          />
-        </div>
-
-       <div className='mt-5'>
-        <button className='bg-green-500 hover:bg-purple-500 w-1/2 p-4
-        text-black-800 font-bold py-2 px-4 border border-black
-        rounded-lg shadow-md mb-4'>{questions[0].correct_answer}
-        </button>
-        <button className='bg-green-500 hover:bg-purple-500 w-1/2 p-4
-        text-black-800 font-bold py-2 px-4 border border-black
-        rounded-lg shadow-md mb-4'>{questions[0].incorrect_answer}
-        </button>
-        <button className='bg-green-500 hover:bg-purple-500 w-1/2 p-4
-        text-black-800 font-bold py-2 px-4 border border-black
-        rounded-lg shadow-md mb-4'>{questions[0].incorrect_answer}
-        </button>
-        <button className='bg-green-500 hover:bg-purple-500 w-1/2 p-4
-        text-black-800 font-bold py-2 px-4 border border-black
-        rounded-lg shadow-md mb-4'>{questions[0].incorrect_answer}
-        </button>
-       </div>
-     </div>
-   ) : (
-        <h1>....Loading!</h1>
-      ) */}
-
   }
 
 
