5d263f8b32a6fde61269dbfb759d0d54e400ad3e
Christina Williams <christinaxtwilliams@gmail.com>
2020-10-30 20:24:48 -0400

    added changes to map

--- src/components/Trivia.js
+++ src/components/Trivia.js
@@ -1,12 +1,5 @@
 import React from 'react';
 
-const Button = ({ answer, className }) => (
-  <button className={`bg-yellow-500 hover:bg-purple-500 p-4
-  text-black-800 font-bold border border-black
-  rounded-lg shadow-md ${className}`}>
-      {answer}
-  </button>
-);
 
 const Trivia = ({ baseAnswer, data: { question, correct, incorrect } }) => {
     const mixedAnswer = [correct, ...incorrect].sort(() => Math.random() - 0.5);
@@ -20,32 +13,26 @@ const Trivia = ({ baseAnswer, data: { question, correct, incorrect } }) => {
     </div>
 
    <div className='grid grid-cols-2 gap-6 mt-6'>
-     <Button
-       className={correct===mixedAnswer[0] ? 'bg-yellow-500' : ''}
-       onClick={() => baseAnswer
-       (mixedAnswer[0])}
-       answer={mixedAnswer[0]}
-       />
-     <Button
-       className={correct===mixedAnswer[1] ? 'bg-yellow-500' : ''}
-       onClick={() => baseAnswer
-       (mixedAnswer[1])}
-       answer={mixedAnswer[1]}
-       />
-     <Button
-       className={correct===mixedAnswer[2] ? 'bg-yellow-500' : ''}
-       onClick={() => baseAnswer
-       (mixedAnswer[2])}
-       answer={mixedAnswer[2]}
-       />
-     <Button
-       className={correct===mixedAnswer[3] ? 'bg-yellow-500' : ''}
-       onClick={() => baseAnswer
-       (mixedAnswer[3])}
-       answer={mixedAnswer[3]}
-       />
+
+      {mixedAnswer.map(answer => (
+          <button
+              className={`${correct===answer
+                ? 'bg-yellow-500' :
+                'bg-yellow-500'
+               } p-4
+                text-black-800
+                font-bold
+                border border-black
+                rounded-lg shadow-md`}
+                onClick={() => baseAnswer
+              (answer)}>
+              {answer}
+            </button>
+        ))}
+
+      </div>
+
    </div>
- </div>
    );
 };
 
