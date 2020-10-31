5ff56374fadc46453b925a6c3e19e39468dfa3ac
Christina Williams <christinaxtwilliams@gmail.com>
2020-10-31 13:18:12 -0400

    added slice

--- src/App.js
+++ src/App.js
@@ -25,9 +25,26 @@ const App = () => {
          ].sort(() => Math.random() - 0.5),
        }));
 
-        setQuestions(questions);
+        setQuestions(questions.sort(() => Math.random()).slice(0, 10));
       });
   }, []);
+
+  {/* useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+       const questions = data.map((question) =>
+       ({
+         ...question,
+         answers: [
+           question.correct,
+           ...question.incorrect,
+         ].sort(() => Math.random() - 0.5),
+       }));
+
+        setQuestions(questions);
+      });
+  }, []); */}
 
 
   const baseAnswer = (answer) => {
@@ -51,7 +68,7 @@ const handleNewQuestion = () => {
      return null;
    }
 
-    return (
+    {/* return (
             questions.length > 0 ? (
           <div className='container'>
             currentIndex >=questions.length ? (
@@ -71,7 +88,40 @@ const handleNewQuestion = () => {
        ) : (
           <h2 className='text-2xl text-green font-bold'>..The Spiders are Dancing!</h2>
 
-    ))
+    ))  */}
+
+    return (
+        <div>
+          {
+            questions.length > 0
+            ?
+            (
+            <div className='container'>
+              {
+                currentIndex >= questions.length
+              ?
+              (
+              <h1 className="text-3xl text-white-800 font-bold">
+              Your Score: You got {score} out of 10 questions.
+              </h1>
+
+              ) : (
+                  <Trivia
+                  data={questions[currentIndex]}
+                  showAnswers={showAnswers}
+                  handleNewQuestion={handleNewQuestion}
+                  baseAnswer={baseAnswer}
+                  />
+                 )
+               }
+              </div>
+             ) : (
+                <h2 className='text-2xl text-green font-bold'>..The Spiders are Dancing!</h2>
+
+            )
+          }
+        </div>
+    )
   }
 
 
